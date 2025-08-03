a739fb0bf637f80573c6af7b8cce5d32ef4642d2
Siddharth2207 <siddharthbhoite22@gmail.com>
2025-08-04 00:38:41 +0530

    update sg

--- src/lib/network.ts
+++ src/lib/network.ts
@@ -1,5 +1,5 @@
 export const BASE_SFT_SUBGRAPH_URL =
-  "https://api.goldsky.com/api/public/project_cm153vmqi5gke01vy66p4ftzf/subgraphs/sft-offchainassetvaulttest-base/1.0.3/gn";
+  "https://api.goldsky.com/api/public/project_cm153vmqi5gke01vy66p4ftzf/subgraphs/sft-offchainassetvaulttest-base/1.0.4/gn";
 export const BASE_ORDERBOOK_SUBGRAPH_URL =
   "https://api.goldsky.com/api/public/project_clv14x04y9kzi01saerx7bxpg/subgraphs/ob4-base/2024-12-13-9c39/gn";
 export const BASE_METADATA_SUBGRAPH_URL =
